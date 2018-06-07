c4ab2e724eb5f476dc4ed9401143e1496268257c
Joey Cho <cjy12182000@naver.com>
2018-06-07 12:52:03 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,18 @@
 class Genre
+  @@all = []
+
+  attr_accessor :name
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def new_song(name, artist)
+
 
 end
