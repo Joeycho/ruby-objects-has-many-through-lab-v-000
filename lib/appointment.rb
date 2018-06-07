217f357bb8d34d3bf9fa2e93f62adb6aa634059c
Joey Cho <cjy12182000@naver.com>
2018-06-07 13:05:48 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,14 +1,16 @@
 class Appointment
 @@all = []
 
-attr_accessor :name
+attr_accessor :name, :paitient, :doctor
 
 def self.all
   @@all
 end
 
-def initialize(name)
+def initialize(name,paitient,doctor)
   @name = name
+  @paitient = paitient
+  @doctor = doctor
   self.class.all << self
 end
 end
