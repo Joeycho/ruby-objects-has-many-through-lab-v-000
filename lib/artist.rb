af6f0358112f197601fbb272a375dbce42089872
Joey Cho <cjy12182000@naver.com>
2018-06-07 12:43:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,26 @@
 class Artist
+  @@all = []
+
+  attr_accessor :name
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def new_song(name, genre)
+  end
+
+  def songs
+    
+  end
+
+  def genres
+
+  end
 
 end
