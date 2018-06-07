15bcb528c5141dba9d3cf3a5babd0021a16d8050
Joey Cho <cjy12182000@naver.com>
2018-06-07 13:11:26 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,7 +13,7 @@ def initialize(name)
 end
 
   def new_appointment(date, doctor)
-    appointment = Appointment.new(date, self, doctor)
+    appointment = Appointment.new(date,self, doctor)
   end
 
   def appointments
